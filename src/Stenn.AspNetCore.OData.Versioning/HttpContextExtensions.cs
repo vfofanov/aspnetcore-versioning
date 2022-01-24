1eb70cc36090f9ac5d0ac5b46b4d39797fccbaff
Vasiliy Fofanov <vasiliy.fofanov@stenn.com>
2022-01-24 16:33:38 +0300

    Add odata unauthorized middleware

---
+++ src/Stenn.AspNetCore.OData.Versioning/HttpContextExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace Stenn.AspNetCore.OData.Versioning
+{
+    /// <summary>
+    ///     Provides extension methods for the <see cref="HttpContext" />.
+    /// </summary>
+    public static class HttpContextExtensions
+    {
+        /// <summary>
+        ///     Return the <see cref="IODataAuthorizedFeature" /> from the <see cref="HttpContext" />.
+        /// </summary>
+        /// <param name="httpContext">The <see cref="HttpContext" /> instance to extend.</param>
+        /// <param name="statusCode"></param>
+        /// <returns>The <see cref="IODataAuthorizedFeature" />.</returns>
+        public static IODataAuthorizedFeature AddODataAuthorizedFeature(this HttpContext httpContext, int? statusCode = null)
+        {
+            if (httpContext == null)
+            {
+                throw new ArgumentNullException(nameof(httpContext));
+            }
+
+            var feature = httpContext.Features.Get<IODataAuthorizedFeature?>();
+            if (feature == null)
+            {
+                feature = new ODataAuthorizedFeature();
+                httpContext.Features.Set(feature);
+            }
+            if (statusCode != null)
+            {
+                feature.StatusCode = statusCode.Value;
+            }
+            return feature;
+        }
+
+        /// <summary>
+        ///     Return the <see cref="IODataAuthorizedFeature" /> from the <see cref="HttpContext" />.
+        /// </summary>
+        /// <param name="httpContext">The <see cref="HttpContext" /> instance to extend.</param>
+        /// <returns>The <see cref="IODataAuthorizedFeature" />.</returns>
+        public static IODataAuthorizedFeature? GetODataAuthorizedFeature(this HttpContext httpContext)
+        {
+            if (httpContext == null)
+            {
+                throw new ArgumentNullException(nameof(httpContext));
+            }
+            return httpContext.Features.Get<IODataAuthorizedFeature?>();
+        }
+    }
+}
---
+++ src/Stenn.AspNetCore.OData.Versioning/IODataAuthorizedFeature.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Stenn.AspNetCore.OData.Versioning
+{
+    /// <summary>
+    /// If this feature exists in <see cref="HttpContext.Features"/> this mean that this is OData unauthorized request
+    /// </summary>
+    public interface IODataAuthorizedFeature
+    {
+        int StatusCode { get; set; }
+    }
+}
