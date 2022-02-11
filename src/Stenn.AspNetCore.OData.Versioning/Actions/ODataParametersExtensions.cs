8d11d0af1aca729cfacb0b3ba85f9b29b02cc1f8
Vasiliy Fofanov <vasiliy.fofanov@stenn.com>
2022-02-12 01:11:33 +0300

    Up version 2.0.5
    Fix null ODataActionParameters mapping behavior

--- src/Stenn.AspNetCore.OData.Versioning/Actions/ODataParametersExtensions.cs
+++ src/Stenn.AspNetCore.OData.Versioning/Actions/ODataParametersExtensions.cs
@@ -9,16 +9,12 @@ namespace Stenn.AspNetCore.OData.Versioning.Actions
     public static class ODataParametersExtensions
     {
         private static readonly ConcurrentDictionary<Type, IODataActionParametersMapper> ParamsMappers = new();
-        public static T Get<T>(this Dictionary<string, object> parameters)
+
+        public static T Get<T>(this Dictionary<string, object>? parameters)
             where T : ODataActionParams, new()
         {
-            if (parameters == null)
-            {
-                throw new ArgumentNullException(nameof(parameters), "Parameters are null. Check action's parameters names and missed parameters");
-            }
-
             var mapper = ParamsMappers.GetOrAdd(typeof(T), ODataActionParametersMapper<T>.Create);
-            return (T)mapper.Map(parameters);
+            return (T)mapper.Map(parameters ?? new Dictionary<string, object>());
         }
     }
 }
